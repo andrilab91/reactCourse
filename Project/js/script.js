90c06a6e2da592c5752d7702ba80df01284872bc
Andrii <Andrii.labunskyi@gmail.com>
2020-11-29 01:31:53 +0200

    practise 1 done

--- Project/js/script.js
+++ Project/js/script.js
@@ -1,7 +1,7 @@
 "use strict";
-let numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
 
-let personalMovieDB = {
+const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
     actors: {},
@@ -10,10 +10,12 @@ let personalMovieDB = {
 };
 
 
-let watched = prompt('Один из последних просмотренных фильмов?', '');
-let rating = prompt('На сколько оцените его?', '');
-let movies = {};
-movies.watched1 = watched;
-movies.rating1 = rating;
+const   a = prompt('Один из последних просмотренных фильмов?', ''),
+        b = prompt('На сколько оцените его?', ''),
+        c = prompt('Один из последних просмотренных фильмов?', ''),
+        d = prompt('На сколько оцените его?', '');
 
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
 
+console.log(personalMovieDB);
