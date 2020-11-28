badd2a5705f865415cb8dcbd48f3e38f68b4f738
Andrii <Andrii.labunskyi@gmail.com>
2020-11-28 23:46:29 +0200

    script updated

--- Project/js/script.js
+++ Project/js/script.js
@@ -1,1 +1,19 @@
 "use strict";
+let numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+};
+
+
+let watched = prompt('Один из последних просмотренных фильмов?', '');
+let rating = prompt('На сколько оцените его?', '');
+let movies = {};
+movies.watched1 = watched;
+movies.rating1 = rating;
+
+
